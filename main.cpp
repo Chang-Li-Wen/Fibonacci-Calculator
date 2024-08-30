284072624aeea4483b32c424d7112d88dd55d904
LiWen Chang <steven890919@gmail.com>
2024-08-30 16:00:17 +0800

    better algo

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 
 int main(){
     std::cout << "Fibo Fibo" << std::endl;
+
+    int n = 40;
+    int a[n+1];
+    a[1] = 1;
+    a[0] = 0;
+    int temp;
+    /* for(int i = 2; i <= n; i++){
+        temp = pre;
+        pre = pre +prepre;
+        prepre = temp;
+    } */
+    for(int i = 2; i <=n; i++){
+        a[i] = a[i-1] + a[i-2];
+   }
+
+    std::cout<< a[n] <<std::endl;
     return 0;
 }
