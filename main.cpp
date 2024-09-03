08493e305ffe64cdb9de7b2ba134aec4c179c506
LiWen Chang <steven890919@gmail.com>
2024-09-03 14:39:42 +0800

    Input from user
    Now the code will ask which Fibo number they want to know and output the result. No longer to modified the code for different Fibo number.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Fibo Fibo" << std::endl;
+    
+    // Ask the user to enter the Fibonaccii number they want to know
+    std::string number;
+    std::cout << "Which Fibonacci number you would like to know?" << std::endl;
+    std::cin >> number;
+    int n = std::stoi(number); // Convert the string into a integer
 
-    int n = 40;
+    // Do the calculation. This method can handle F(0) and F(1) also.
     int a[n+1];
     a[1] = 1;
     a[0] = 0;
-    for(int i = 2; i <=n; i++){
+    for(int i = 2; i <= n; i++){
         a[i] = a[i-1] + a[i-2];
-   }
+    }
 
-    std::cout<< a[n] <<std::endl;
+    // Output the result.
+    std::cout << number + "-th Fibonacci numver is: " << a[n] <<std::endl;
     return 0;
 }
